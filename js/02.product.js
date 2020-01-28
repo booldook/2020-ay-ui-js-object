e354053e90a2ff268321f6704907b7ad6cf57f07
booldook <booldook@gmail.com>
2020-01-28 12:05:41 +0900

    02.product

---
+++ js/02.product.js
@@ -0,0 +1,29 @@
+var Box = (function(){
+	function Box(parent, className, color, hoverColor, width, height) {
+		this.parent = parent;
+		this.className = className;
+		this.color = color;
+		this.hoverColor = hoverColor;
+		this.width = width;
+		this.height = height;
+		this.init();
+	}
+	Box.prototype.init = function(){
+		var me = this;
+		var style = 'width:'+this.width+'px; height:'+this.height+'px; background-color:'+this.color+';';
+		var html = '<div class="'+this.className+'" style="'+style+'"></div>';
+		// var obj = this.parent.append(html); //parent
+		this.obj = $(html).appendTo(this.parent); //children
+		this.obj.mouseenter(function(){
+			$(this).css({"background-color": me.hoverColor});
+		});
+		this.obj.mouseleave(function(){
+			$(this).css({"background-color": me.color});
+		});
+	}
+	return Box;
+})();
+
+// 실행
+var box = new Box($(".wrapper"), "box", "blue", "red", 100, 100);
+var box2 = new Box($(".wrapper"), "box", "orange", "green", 150, 150);
