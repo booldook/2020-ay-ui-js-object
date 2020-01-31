7378f6ba35bdebbf563d25fbf586d8f848e13482
booldook <booldook@gmail.com>
2020-01-31 11:57:21 +0900

    06.lotto 완성

--- js/06.lotto.js
+++ js/06.lotto.js
@@ -1,11 +1,6 @@
 /* 
 <div class="lt-box">
 	<div class="ball-sm color00">1</div>
-	<div class="ball-sm color10">12</div>
-	<div class="ball-sm color10">17</div>
-	<div class="ball-sm color20">21</div>
-	<div class="ball-sm color30">33</div>
-	<div class="ball-sm color40">45</div>
 </div>
 
 <div class="ball color00">1</div>
@@ -23,15 +18,34 @@ document.querySelector("#btSel").addEventListener("click", onSel);
 document.querySelector("#btReset").addEventListener("click", onReset);
 
 function onSel() {
-	var lotto = [];
+	var lotto = [], lotto2 = [];
 	var lottoShuffle = _.shuffle(lottoArr);
 	for(var i=0; i<6; i++) {
 		lotto.push(lottoShuffle.pop());
 	}
 	lotto = _.sortBy(lotto);
+	lotto2 = _.cloneDeep(lotto);
 	console.log(lotto);
-	for(var i in lotto) {
-
+	document.querySelector(".rt-box").innerHTML = '';
+	var html = '', n, c;
+	var interval = setInterval(makeLotto, 500);
+	function makeLotto() {
+		if(lotto.length > 0) {
+			n = lotto.shift();
+			c = Math.ceil(n/10) - 1;
+			html = '<div class="ball color'+c+'0">'+n+'</div>';
+			document.querySelector(".rt-box").innerHTML += html;
+		}
+		else {
+			clearInterval(interval);
+			html = '<div class="lt-box">';
+			for(var i in lotto2) {
+				c = Math.ceil(lotto2[i]/10) - 1;
+				html += '<div class="ball-sm color'+c+'0">'+lotto2[i]+'</div>';
+			}
+			html += '</div>';
+			document.querySelector(".lt-wrap").innerHTML += html;
+		}
 	}
 }
 
